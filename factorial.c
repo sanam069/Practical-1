c3db3901bf9be94edbd32cc3a8108c902a54c107
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 15:26:54 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,16 @@
+**To find factorial of a number**
+
+#include<stdio.h>  
+int main()  
+{  
+int i,n,fact=1;  
+printf("Enter the  number whose factorial is to be find :\n");  
+scanf("%d",&n);  
+for(i=1;i<=n;i++)  
+{  
+fact=fact*i;  
+}  
+printf("Factorial of given number is : %d \n",fact);  
+return 0;  
+}  
+  

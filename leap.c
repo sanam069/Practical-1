1e7d4dafbabf1a3e4c657232d70104477adb212d
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 15:31:45 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,16 @@
+**To check whether a year is leap or not**
+
+#include<stdio.h>  
+int main()  
+{  
+int year;  
+printf("Enter the value of year");  
+scanf("%d",&year);  
+if(year%4==0)  
+printf(" The year is Leap \n");  
+else  
+printf(" \n The year is not leap \n");  
+return 0;  
+}  
+
+                                                                          

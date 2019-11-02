b68bfb69a24c3b197c03e5fe84bfe78da8c2f286
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 15:34:26 +0530

    Create sumoffirst100numbers.c

---
+++ sumoffirst100numbers.c
@@ -0,0 +1,13 @@
+**To find sum of first 100 positive integers**
+
+#include<stdio.h>  
+int main()  
+{  
+int i,sum=0;  
+for(i=1;i<=100;i++)  
+{                                                                         
+sum=sum+i;  
+}  
+printf("Sum of first 100 numbers is %d \n",sum);  
+return 0;  
+}  

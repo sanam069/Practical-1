3fddfe05b0ec91d5fe24a6e97bc4a2a462640d72
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 15:01:07 +0530

    Create colour.c

---
+++ colour.c
@@ -0,0 +1,29 @@
+To print colour by selectin colour code
+
+#include<stdio.h>  
+int main()  
+{  
+char code;  
+printf("Enter y for yellow, r for red, b for blue and g for green ");  
+scanf("%c",&code);  
+switch (code)  
+{  
+case 'y':  
+printf("Yellow \n");  
+break;  
+case 'b':  
+printf("Blue \n");  
+break;  
+case 'g':  
+printf("Green \n");  
+break;  
+case 'r':  
+printf("Red \n");  
+break;  
+default:                                                                  
+printf("Wrong choice \n");  
+break;  
+}  
+return 0;  
+}  
+                                                                          

f1ad3e1289348726a1d41fe62b3afb3f804bc778
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 15:29:24 +0530

    Create greateroftwo.c

---
+++ greateroftwo.c
@@ -0,0 +1,18 @@
+**To find greater of two numbers**
+
+#include<stdio.h>  
+int main()  
+{  
+int a,b;  
+printf("Enter the  value of a and b");  
+scanf("%d%d",&a,&b);  
+if(a>b)  
+{  
+printf("a is grteater");  
+}  
+else  
+{  
+printf("b is greater");  
+}                                                                         
+return 0;  
+}  

60e70db7296fb4bbe8826b87140ccc9ca6c8edd1
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 16:05:03 +0530

    Create identitymatrix.c

---
+++ identitymatrix.c
@@ -0,0 +1,19 @@
+**  To print identity matrix of 3*3  **
+
+#include<stdio.h>  
+void main()  
+{  
+int a[3][3],b[3][3],i,j;  
+printf("The elements of 3 * 3 identity matrix:\n");  
+for(i=0;i<3;i++)  
+{  
+for(j=0;j<3;j++)  
+{  
+if(i==j)  
+printf(" 1 ");  
+else  
+printf(" 0 ");                                                            
+}  
+printf("\n");  
+}  
+}  

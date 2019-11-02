cacd660df68e1d312bcfa6d1fa7f805be85e0e86
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-02 14:54:03 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,23 @@
+"To check whether a number is armstrong or not"  
+
+#include<stdio.h>  
+int main()  
+{  
+int number,temp;  
+int arm=0,digit;  
+printf("Enter any number");  
+scanf("%d",&number);  
+temp=number;  
+while(temp>0)  
+{  
+digit=temp%10;  
+arm=arm+(digit*digit*digit);  
+temp/=10;   
+}   
+if(arm==number)  
+printf("Armstrong\n");  
+else  
+printf("Not armstrong\n");  
+  
+return 0;  
+}  

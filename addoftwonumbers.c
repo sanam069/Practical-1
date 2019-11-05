bf1ab39c1870e65cea614f3ab0ffedc1db363187
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-05 23:43:36 +0530

    Create addoftwonumbers.c

---
+++ addoftwonumbers.c
@@ -0,0 +1,12 @@
+**To find sum of two numbers **
+
+#include<stdio.h>
+int main()
+{
+int a,b,sum=0;
+printf("Enter two numbers \n");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("Sum of two numbers is %d \n",sum);
+return 0;
+}

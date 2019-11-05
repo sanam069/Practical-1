9d94deefdb55f3dc5b226ba077a009c735be00de
Tushar Garg <54389414+gargtushar2000@users.noreply.github.com>
2019-11-05 23:45:25 +0530

    Create avgofn_numbers.c

---
+++ avgofn_numbers.c
@@ -0,0 +1,19 @@
+**To find average of n numbers**
+
+#include<stdio.h>
+int main()
+{
+int n,i,a[10];
+int sum=0,avg;
+printf("Enter number of elements: \n");
+scanf("%d",&n);
+printf("Enter %d elements: \n ",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+avg=sum/n;
+printf("Average is %d \n",avg);
+return 0;                                                                 
+}
